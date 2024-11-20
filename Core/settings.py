1f08e48f554fae61f68bdca9b9d0c82154f688ab
Kareem <kareemkv1990@yahoo.com>
2024-11-20 12:44:05 +0000

    debug is false

--- Core/settings.py
+++ Core/settings.py
@@ -28,7 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'django-backend-template-c4624bfe6451.herokuapp.com', 
