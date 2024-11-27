dcbaa021864b7c30f0afe63dde91bcd032d9206e
Kareem <kareemkv1990@yahoo.com>
2024-11-27 14:53:15 +0000

    url fix

--- Core/urls.py
+++ Core/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+# For passowrd reset function
+from django.contrib.auth import views as auth_views
 
 schema_view = get_schema_view(
     openapi.Info(
