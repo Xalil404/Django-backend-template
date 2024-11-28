c77eafa13affb577ed12c7a1cf6d65b673a05df5
Xalil404 <kareemxalil@gmail.com>
2024-11-28 11:15:02 +0000

    password reset

--- Core/urls.py
+++ Core/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+# for password reset
+from django.contrib.auth.views import PasswordResetView
 
 
 
@@ -43,6 +44,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('auth/', include('dj_rest_auth.urls')),  # Login/logout/password reset
     path('auth/registration/', include('dj_rest_auth.registration.urls')),  # Registration
+    path('auth/password/reset/', PasswordResetView.as_view(email_template_name='password_reset_email.html'), name='password_reset'),
     path('api/', include('Contact.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
